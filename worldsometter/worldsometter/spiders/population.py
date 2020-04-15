74bc906e374a3868fb732ec7e4fff9dee3a27dbc
nilsjanvier <60745167+nilsjanvier@users.noreply.github.com>
2020-04-15 17:45:25 +0200

    Add files via upload

--- worldsometter/worldsometter/spiders/population.py
+++ worldsometter/worldsometter/spiders/population.py
@@ -6,10 +6,12 @@ class PopulationSpider(scrapy.Spider):
     name = 'population'
     allowed_domains = ['https://www.worldometers.info']
     start_urls = ['https://www.worldometers.info/coronavirus/']
+    custom_settings={ 'FEED_URI': "data/covid19_worldometers_%(time)s.json",
+                       'FEED_FORMAT': 'json'}
     
     def parse(self, response):
-        
-        for each in response.xpath('//*[@id="main_table_countries_yesterday"]/tbody/tr'):
+
+        for each in response.xpath('//*[@id="main_table_countries_today"]/tbody/tr'):
 	        yield {
 	            'country': each.xpath('td[1]//text()').extract(),
 	            'total_cases': each.xpath('td[2]//text()').extract(),
@@ -17,5 +19,6 @@ class PopulationSpider(scrapy.Spider):
 	            'total_deaths': each.xpath('td[4]//text()').extract(),
 	            'new_deaths': each.xpath('td[5]//text()').extract(),
 	            'total_recovered': each.xpath('td[6]//text()').extract(),
-	            'date' : date.today() - timedelta(days=1)
+	            'date' : date.today()
+	            # 'date' : date.today() - timedelta(days=1)
 	        }
